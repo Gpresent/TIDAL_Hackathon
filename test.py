728947ceef08b0c1c098feff1c71c80b1f5dc20e
Gage Broberg <gagebroberg@tamu.edu>
2023-11-11 14:18:19 -0600

    Outputting model.

--- test.py
+++ test.py
@@ -1,24 +1,20 @@
-
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 from flask import Flask
+import json
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
+# Function to load data from JSON file
+def load_json_data(filename):
+    with open(filename) as file:
+        return json.load(file)
+
+# Load data once when the app starts
+json_data = load_json_data('tensorflowjs_files/model.json')
+
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World'
+    # Use data from JSON file
+    return json_data
 
-
-# main driver function
 if __name__ == '__main__':
-
-    # run() method of Flask class runs the application
-    # on the local development server.
     app.run()
