9288299f0635d2f756e09b8bdd8f6a7012719595
gpresent <gpresent@tamu.edu>
2023-11-11 10:26:25 -0600

    Wacky changes

--- cole_file.py
+++ cole_file.py
@@ -1,63 +1,49 @@
-import tensorflow as tf
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 if __name__ == '__main__':
 
     tidal_train = pd.read_csv("./DATA/DATA.csv")
 
-    tidal_features = tidal_train.copy()
-    tidal_labels = tidal_features.pop('GRADE')
-    tidal_features.pop('STUDENT ID')
+    tidal_features = tidal_train.drop(['GRADE', 'STUDENT ID'], axis=1)
+    tidal_labels = tidal_train.pop('GRADE')
+    print(tidal_labels)
 
     tidal_features = np.array(tidal_features)
-
+    tidal_labels = np.array(tidal_labels)
     tidal_features = tidal_features.astype(np.int32)  # Convert to tf.float32
     tidal_labels = tidal_labels.astype(np.int32)  # Convert to tf.int32
 
-    normalize = tf.keras.layers.Normalization()
-    normalize.adapt(tidal_features)
+    # Split the data into training and testing sets
+    # Adjust test_size and random_state as needed
+    features_train, features_test, labels_train, labels_test = train_test_split(
+        tidal_features, tidal_labels, test_size=0.2, random_state=42, stratify=tidal_labels)
+    print(features_train.shape, features_test.shape, labels_train.shape, labels_test.shape)
+    categorical_features = range(tidal_features.shape[1])
 
-    tidal_model = tf.keras.Sequential([
-        normalize,
-        tf.keras.layers.Dense(64),
-        tf.keras.layers.Dense(1)
+    # One-hot encode categorical features
+    encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+    features_train_encoded = encoder.fit_transform(features_train)
+    features_test_encoded = encoder.transform(features_test)
+
+    # Build the model
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(features_train_encoded.shape[1],)),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')  # Assuming 10 classes, adjust accordingly
     ])
 
-    tidal_model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=tf.keras.optimizers.Adam())
+    # Compile the model
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['categorical_accuracy'])
 
-    tidal_model.fit(tidal_features, tidal_labels, epochs=10)
+    # Train the model
+    model.fit(features_train_encoded, labels_train, epochs=10, batch_size=32, validation_split=0.2)
 
-    # # Prepare the input data for prediction
-    # student_data = data.iloc[:, 1:-2].values  # Extract features for each student
-    # course_data = data["COURSE ID"].values  # Extract the course ID
-    # input_data = np.column_stack((student_data, course_data))  # Combine features and course ID
-    #
-    # # Make predictions
-    # predictions = model.predict(input_data)
-    #
-    # # Add the predictions to the DataFrame
-    # data["PREDICTED GRADE"] = predictions
-    #
-    # # Save the DataFrame with predictions to a new CSV file
-    # data.to_csv("predictions.csv", index=False)
-    # print(tidal_train.head())
-
-    # mnist = tf.keras.datasets.mnist
-    # (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    # x_train, x_test = x_train / 255.0, x_test / 255.0
-    # model = tf.keras.models.Sequential([
-    #     tf.keras.layers.Flatten(input_shape=(28, 28)),
-    #     tf.keras.layers.Dense(128, activation='relu'),
-    #     tf.keras.layers.Dropout(0.2),
-    #     tf.keras.layers.Dense(10)
-    # ])
-    # predictions = model(x_train[:1]).numpy()
-    # tf.nn.softmax(predictions).numpy()
-    # loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    # loss_fn(y_train[:1], predictions).numpy()
-    # model.compile(optimizer='adam',
-    #               loss=loss_fn,
-    #               metrics=['accuracy'])
-    # model.fit(x_train, y_train, epochs=5)
-    # model.evaluate(x_test, y_test, verbose=2)
+    # Evaluate the model on the test set
+    test_loss, test_accuracy = model.evaluate(features_test_encoded, features_test)
+    print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
